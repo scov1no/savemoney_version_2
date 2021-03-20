a8f3162b5de23675e67595bb06daade687b82539
Edmilson Scovino <scovinowork@gmail.com>
2021-03-20 10:45:30 -0300

    ARQUIVO JAVA TEST COM BASE AO DIAGRAMA DO GUSTAVO SHADE

---
+++ arquivo_java_test/src/croud_test_java/SM_USUARIO_RECEITA.java
@@ -0,0 +1,8 @@
+package croud_test_java;
+
+public class SM_USUARIO_RECEITA extends SM_Usuario{
+    private String ID_USUARIO_RECEITA;
+    private String SM_USUARIO_ID_USUARIO;
+    private String SM_RECEITA_ID_RECEITA;
+
+}
