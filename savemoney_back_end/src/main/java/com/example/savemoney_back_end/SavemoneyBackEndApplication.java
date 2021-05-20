9802b7d84c3db0f59e17f3888188c59de928e171
Edmilson Scovino <scovinowork@gmail.com>
2021-05-20 00:05:43 -0300

    Ajustes index

---
+++ savemoney_back_end/src/main/java/com/example/savemoney_back_end/SavemoneyBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.savemoney_back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavemoneyBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SavemoneyBackEndApplication.class, args);
+	}
+
+}
---
+++ savemoney_back_end/src/test/java/com/example/savemoney_back_end/SavemoneyBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.savemoney_back_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavemoneyBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
