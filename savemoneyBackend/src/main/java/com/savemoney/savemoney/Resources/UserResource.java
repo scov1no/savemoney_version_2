4f652a0c162b8e8b05c1cb3d2cbc00f729916b2c
Edmilson Scovino <scovinowork@gmail.com>
2021-05-20 14:57:02 -0300

    Mexendo no ResController do User

--- savemoneyBackend/src/main/java/com/savemoney/savemoney/Resources/UserResource.java
+++ savemoneyBackend/src/main/java/com/savemoney/savemoney/Resources/UserResource.java
@@ -23,10 +23,7 @@ public class UserResource {
 
     private ResponseEntity<User> responseEntity;
 
-    @GetMapping("/api/listarvalores")
-    public List<User> listar(){
-        return UserRepository.findAll();
-    }
+
 
 
 }
