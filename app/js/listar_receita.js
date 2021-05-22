2a59e778f656650fbf1ac2e5fffdd94a9c3d7a95
Edmilson Scovino <scovinowork@gmail.com>
2021-05-21 22:20:45 -0300

    Página de receita tbm agora listar

--- app/js/listar_receita.js
+++ app/js/listar_receita.js
@@ -16,6 +16,21 @@ $(document).ready(function (){
         }
     })
 });
+$(document).ready(function (){
+    $.ajax({
+        url:'http://localhost:8080/api/receita/listar',
+        type:'get',
+        dataType: 'json',
+        success: function(result){
+            var html = '';
+            $.each(result, function (i, data){
+                html += '<tr><td>' + data.nome_receita +'</td>';
+                html += '<td>R$ ' + data.valor_receita +'</td></tr>';
+                $("#tabela_listar_aluno_tbody_1").html(html);
+            });
+        }
+    })
+});
 
 /*
 function removerAluno(id_receita){
