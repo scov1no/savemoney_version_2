3f355918be8052f481fa08e439529b7cfcefdca9
Edmilson Scovino <scovinowork@gmail.com>
2021-05-22 21:23:07 -0300

    Metodo Incluir

--- app/js/adicionar_receita.js
+++ app/js/adicionar_receita.js
@@ -1,8 +1,11 @@
 //Processar Formulario
-$ ('#form-inserir-usuario').submit(function (event){
+$ ('#form-adicionar').submit(function (event){
     event.preventDefault();
     //Criar formData
-    var formData = {'nome': $('#input-nome').val() };
+    var formData = {'nome': $('#input-nome_receita').val(),
+                    'valor': $('#input-valor_receita').val(),
+                    'descricao': $('#input-descricao_receita').val()
+    };
 
     $.ajax({
         headers : {
@@ -10,7 +13,7 @@ $ ('#form-inserir-usuario').submit(function (event){
             'Content-Type' : 'application/json'
         },
         type: 'POST',
-        url: 'http://localhost:8080/api/aluno/incluir',
+        url: 'http://localhost:8080/api/receita/incluir',
         data: JSON.stringify(formData),
         dataType: 'json',
         encode: true,
@@ -25,3 +28,4 @@ $ ('#form-inserir-usuario').submit(function (event){
 
     });
 });
+
--- savemoneyBackend/src/main/java/com/savemoney/savemoney/Entities/Receita.java
+++ savemoneyBackend/src/main/java/com/savemoney/savemoney/Entities/Receita.java
@@ -1,6 +1,7 @@
 package com.savemoney.savemoney.Entities;
 
 import java.io.Serializable;
+import java.text.DateFormat;
 import java.time.Instant;
 
 import javax.persistence.Entity;
@@ -21,7 +22,7 @@ public class Receita{
     private String nome_receita;
     private float valor_receita;
     private String descricao_receita;  
-    private Instant data_receita;
+    private DateFormat data_receita;
 
     /*
     @ManyToOne
@@ -65,11 +66,11 @@ public class Receita{
         this.descricao_receita = descricao_receita;
     }
 
-    public Instant getData_receita() {
+    public DateFormat getData_receita() {
         return data_receita;
     }
 
-    public void setData_receita(Instant data_receita) {
+    public void setData_receita(DateFormat data_receita) {
         this.data_receita = data_receita;
     }
     /*
