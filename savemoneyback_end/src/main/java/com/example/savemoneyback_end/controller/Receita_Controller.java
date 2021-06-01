35eb3e40a49bf512f513aee1d5cae8a0f515ff55
Edmilson Scovino <scovinowork@gmail.com>
2021-06-01 19:56:35 -0300

    Erro resolvido do postman

--- savemoneyback_end/src/main/java/com/example/savemoneyback_end/controller/Receita_Controller.java
+++ savemoneyback_end/src/main/java/com/example/savemoneyback_end/controller/Receita_Controller.java
@@ -11,12 +11,12 @@ public class Receita_Controller {
         }
     }
 
-    /** Verifica se o nome do aluno é válido.
+    /* Verifica se o nome do aluno é válido.
      *
      * Um nome válido não pode ser nulo e deve ter mais que 3 caracteres
-     *
-     * @param receita
-     * @return
+
+      @param receita
+      @return
      */
     private boolean isNomeValido(Receita receita) {
         if ((receita.getNome_receita().isEmpty()) || (receita.getNome_receita().length() < 3)) {
