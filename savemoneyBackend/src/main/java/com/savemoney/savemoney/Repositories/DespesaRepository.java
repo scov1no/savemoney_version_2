f48a9e93b278d44ce7ceb7d00fec3652ee875a04
mnshade <47904144+mnshade@users.noreply.github.com>
2021-06-14 16:20:03 -0300

    Delete savemoneyBackend directory

--- savemoneyBackend/src/main/java/com/savemoney/savemoney/Repositories/DespesaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.savemoney.savemoney.Repositories;
-
-import com.savemoney.savemoney.Entities.Despesa;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DespesaRepository extends JpaRepository<Despesa, Long>{
-    
-}
